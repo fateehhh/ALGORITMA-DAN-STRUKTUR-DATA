85a83131cb31401c41747ad85a7200da1f9da046
fateehhh <159910664+fateehhh@users.noreply.github.com>
2024-02-14 13:36:35 +0700

    Update PraktikumFungsi

--- src/jobsheet1/PraktikumFungsi.java
+++ src/jobsheet1/PraktikumFungsi.java
@@ -1,14 +1,12 @@
 package jobsheet1;
 
-import java.util.Scanner;
-
 public class PraktikumFungsi {
     public static void main(String[] args) {
         int[][] stock = {
-            {10, 5, 15, 7}, // Aglonema, Keladi, RoyalGarden 1, RoyalGarden 2
-            {6, 11, 9, 12}, // Aglonema, Keladi, RoyalGarden 1, RoyalGarden 4
-            {2, 10, 10, 5}, // Aglonema, Alocasia, RoyalGarden 3, RoyalGarden 4
-            {5, 7, 12, 9}  // Keladi, Mawar, RoyalGarden 1, RoyalGarden 2
+            {10, 5, 15, 7}, 
+            {6, 11, 9, 12}, 
+            {2, 10, 10, 5}, 
+            {5, 7, 12, 9} 
         };
         
         double[] harga = {75_000, 50_000, 60_000, 10_000};
