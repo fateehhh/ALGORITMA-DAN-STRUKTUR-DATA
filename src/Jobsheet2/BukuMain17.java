f6828424dbdddb92ec147a7a25fb12335d021880
fateehhh <siyaapppp@gmail.com>
2024-02-20 10:11:42 +0700

    Percobaan3_Buku17

--- src/Jobsheet2/BukuMain17.java
+++ src/Jobsheet2/BukuMain17.java
@@ -13,6 +13,14 @@ public class BukuMain17 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.TampilInformasi();
+
+        Buku17 bk2 = new Buku17("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.TampilInformasi();
+
+        Buku17 bukuFateh = new Buku17("Pengantar Java", "Riandi Gunawan", 300, 20, 80000);
+        bukuFateh.terjual(5);
+        bukuFateh.TampilInformasi();
     }
 
 }
