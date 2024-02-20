34c873a7212be5ba6297a655a170b8bf89d30cd2
fateehhh <siyaapppp@gmail.com>
2024-02-20 08:24:03 +0700

    Buku17.java

---
+++ src/Jobsheet2/Buku17.java
@@ -0,0 +1,26 @@
+package Jobsheet2;
+
+class Buku {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void TampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
