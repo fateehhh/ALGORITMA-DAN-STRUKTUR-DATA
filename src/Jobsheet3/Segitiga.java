0ccc3f8b4607b858bc91f72f9e29d93c3e448f4e
fateehhh <siyaapppp@gmail.com>
2024-03-01 20:45:21 +0700

    Jobsheet3_Percobaan3_Pertanyaan4

--- src/Jobsheet3/Segitiga.java
+++ src/Jobsheet3/Segitiga.java
@@ -17,4 +17,11 @@ public class Segitiga {
         double keliling = alas + tinggi + hipotenusa;
         return keliling;
     }
+    public static void main(String[] args) {
+        Segitiga[] sgArray = new Segitiga[4];
+        sgArray[0] = new Segitiga(10, 4);
+        sgArray[1] = new Segitiga(20, 10);
+        sgArray[2] = new Segitiga(15, 6);
+        sgArray[0] = new Segitiga(25, 10);
+    }
 }
