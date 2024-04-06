af32e7e97b9d3bd9e696b047639a7687fbc6bca4
fateehhh <siyaapppp@gmail.com>
2024-04-06 12:11:28 +0700

    Jobsheet 6 - Percobaan 1

---
+++ src/Jobsheet6/TestSearching/src/BukuMain17.java
@@ -0,0 +1,45 @@
+package Jobsheet6.TestSearching.src;
+
+import java.util.Scanner;
+
+public class BukuMain17 {
+    public static void main(String[] args) {
+        Scanner s17 = new Scanner(System.in);
+        Scanner s117 = new Scanner(System.in);
+
+        PencarianBuku17 data = new PencarianBuku17();
+        int jumBuku = 5;
+
+        System.out.println("=======================================================");
+        System.out.println("|Masukkan data Buku secara urut dari KodeBuku Terkecil|");
+        for (int i = 0; i < jumBuku; i++) {
+            System.out.println("--------------------");
+            System.out.print("Kode Buku: ");
+            int kodeBuku =  s17.nextInt();
+            System.out.print("Judul Buku: ");
+            String judulBuku = s117.nextLine();
+            System.out.print("Tahun Terbit: ");
+            int tahunTerbit = s17.nextInt();
+            System.out.print("Pengarang: ");
+            String pengarang = s117.nextLine();
+            System.out.print("Stock: ");
+            int stock = s17.nextInt();
+
+            Buku17 m = new Buku17(kodeBuku, judulBuku, tahunTerbit, pengarang, stock);
+            data.tambah(m);
+        }
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Data keseluruhan buku : ");
+        data.tampil();
+
+        System.out.println("_______________________________________________________");
+        System.out.println("_______________________________________________________");
+        System.out.println("Pencarian Data");
+        System.out.println("Mauskkan kode buku yang dicari: ");
+        System.out.print("Kode Buku: ");
+        int cari = s17.nextInt();
+        System.out.println("Menggunakan Sequential Search");
+        int posisi = data.FindSeqSearch(cari);
+        data.TampilData(cari, posisi);
+    }
+}
---
+++ src/Jobsheet6/TestSearching/src/PencarianBuku17.java
@@ -0,0 +1,43 @@
+package Jobsheet6.TestSearching.src;
+
+public class PencarianBuku17 {
+    Buku17 ListBk[] = new Buku17[5];
+    int idx;
+
+    void tambah(Buku17 m) {
+        if (idx < ListBk.length) {
+            ListBk[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+    void tampil(){
+        for (Buku17 m : ListBk) {
+            m.tampilDataBuku();
+        }
+    }
+    public int FindSeqSearch(int cari){
+        int posisi = 0;
+        for (int j = 0; j < ListBk.length; j++) {
+            if (ListBk[j].kodeBuku == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+    public void TampilData (int x, int pos){
+        if (pos != -1) {
+            System.out.println("data: " + x + " ditemukan pada index " + pos);
+            System.out.println("==================================");
+            System.out.println("Kode Buku: " + x);
+            System.out.println("Judul: " + ListBk[pos].judulBuku);
+            System.out.println("Tahun Terbit: " + ListBk[pos].tahunTerbit);
+            System.out.println("Pengarang: " + ListBk[pos].pengarang);
+            System.out.println("Stock: " + ListBk[pos].stock);
+        } else {
+            System.out.println("data: " + x + "tidak ditemukan");
+        }
+    }
+}
