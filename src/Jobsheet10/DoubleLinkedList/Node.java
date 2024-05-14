524f78f94d1d42f7a41145dd2b3e599d22666f8f
fateehhh <siyaapppp@gmail.com>
2024-05-14 10:01:16 +0700

    Jobsheet10 - Double Linked List

---
+++ src/Jobsheet10/DoubleLinkedList/Node.java
@@ -0,0 +1,12 @@
+package Jobsheet10.DoubleLinkedList;
+
+public class Node {
+    int data;
+    Node prev, next;
+
+    Node (Node prev, int data, Node next){
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
