01c2e1cf4107c4ba7ec1213e9d77e3890decdd35
fateehhh <siyaapppp@gmail.com>
2024-05-31 15:47:30 +0700

    Jobsheet11 - Tree -Percobaan 1

---
+++ src/Jobsheet11/Node17.java
@@ -0,0 +1,16 @@
+package Jobsheet11;
+
+public class Node17 {
+    int data;
+    Node17 left;
+    Node17 right;
+    
+    public Node17 (){
+
+    }
+    public Node17 (int data){
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
